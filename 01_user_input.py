96fa441dc0f4c1dae3cbc85a86f3ffb814ab23bc
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2021-04-08 14:54:46 +1200

    WIP user input

--- 01_user_input.py
+++ 01_user_input.py
@@ -16,3 +16,38 @@ def int_check(question, low=None, high=None):
         situation = "both"
     elif low is not None and high is None:
         situation = "low only"
+
+    while True:
+
+        try:
+            response = int(input(question))
+
+            # checks input is not too high or
+            # too low if both upper and lower bounds
+            # are specified
+            if situation == "both":
+                if response < low or response > high:
+                    print("Please enter a number between {} and {}".format(low, high))
+                    continue
+
+            # checks input is not too low
+            elif situation == "low only":
+                if response < low:
+                    print("Please enter a number that is more than (or equal to) {}".format(low))
+                    continue
+
+            return response
+
+        # checks input is an integer
+        except ValueError:
+            print("Please enter an integer")
+            continue
+
+
+# Main routine
+
+
+lowest = int_check("Low Number: ")
+highest = int_check("High Number: ", lowest + 1)
+rounds = int_check("Rounds: ", 1)
+guess = int_check("Guess: ", lowest, highest)
